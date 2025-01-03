129c9dbec9f7b2e584708a9d3cd7eb8f517d4614
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2025-01-03 19:22:57 -0300

    Agrega informacion e imagen de pokemon

--- src/main.js
+++ src/main.js
@@ -1,71 +1,95 @@
-let offset = 0
+let offset = 0;
 
 fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`)
     .then(response => response.json())
     .then(data => {
 
-        const ul = document.getElementById("pokemon-lista")
+        const ul = document.getElementById("pokemon-lista");
 
         data.results.forEach(pokemon => {
-        const li = document.createElement('li'); 
-        li.textContent = pokemon.name;
-        ul.appendChild(li);
+            const li = document.createElement('li');
+            li.textContent = pokemon.name;
+
+            li.addEventListener('click', () => {
+                mostrarDetallesPokemon(pokemon.url);
+            });
+
+            ul.appendChild(li);
         });
 
     })
     .catch(error => console.error('Error:', error));
 
-
-
-function obtenerLista(url){
+function obtenerLista(url) {
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
+        .then(response => response.json())
+        .then(data => {
 
-        const ul = document.getElementById("pokemon-lista")
+            const ul = document.getElementById("pokemon-lista");
+            ul.textContent = "";
 
-        data.results.forEach(pokemon => {
-        const li = document.createElement('li'); 
-        li.textContent = pokemon.name;  
-        ul.appendChild(li);
-        });
-    
-    })
-    .catch(error => console.error('Error:', error));
+            data.results.forEach(pokemon => {
+                const li = document.createElement('li');
+                li.textContent = pokemon.name;
 
-    agregarEventosAClick();
+                li.addEventListener('click', () => {
+                    mostrarDetallesPokemon(pokemon.url);
+                });
 
+                ul.appendChild(li);
+            });
+
+        })
+        .catch(error => console.error('Error:', error));
 }
 
+function mostrarDetallesPokemon(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            const pokemonImagen = document.getElementById("pokemonImagen");
+            const pokemonDetailContainer = document.querySelector(".pokemon-contenedor-detalles h2");
+            const pokemonNumero = document.getElementById("pokemon-numero");
+            const pokemonTipo = document.getElementById("pokemon-tipo");
+
+            
+            pokemonImagen.src = data.sprites.front_default; 
+            pokemonImagen.alt = data.name; 
+            pokemonDetailContainer.textContent = `Detalles de ${data.name}`; 
+            pokemonNumero.textContent = `Número de Pokédex: ${data.id}`
+            const tipos = data.types.map(typeInfo => typeInfo.type.name).join(', ');
+            pokemonTipo.textContent = `Tipo: ${tipos}`;
+
+            
+        })
+        .catch(error => console.error('Error:', error));
+}
 
 const botonPrevio = document.getElementById('boton-previo');
-const botonSiguiente = document.getElementById('boton-siguiente')
+const botonSiguiente = document.getElementById('boton-siguiente');
 
 function manejarClickPrevio() {
-
     const listaPokemon = document.getElementById("pokemon-lista");
-    listaPokemon.textContent = ""
-    
-    if (offset !== 0){
-        offset = offset - 20
+    listaPokemon.textContent = "";
+
+    if (offset !== 0) {
+        offset = offset - 20;
     }
 
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`; // Cambiar la URL según el estado
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`;
     obtenerLista(url);
 }
 
 function manejarClickSiguiente() {
-
     const listaPokemon = document.getElementById("pokemon-lista");
     listaPokemon.textContent = "";
 
     offset = offset + 20;
 
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`; // Cambiar la URL según el estado
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`;
     obtenerLista(url);
 }
 
 botonPrevio.addEventListener('click', manejarClickPrevio);
 botonSiguiente.addEventListener('click', manejarClickSiguiente);
-
-
