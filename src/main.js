cb41d35bc049ea0196526933932ceaa7a25b8599
Santino Mayan <sanpa33@users.noreply.github.com>
2025-09-08 16:45:53 -0300

    Modifica logica principal con funcionamiento del modulo ui

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 let offset = 0;
 
-import { obtenerListaPokemones } from "./pokemones.js";
+import { obtenerListaPokemones } from "./ui/interfaz.js";
 import { manejarClickPrevio, manejarClickSiguiente } from "./ui.js";
 
 const botonPrevio = document.getElementById('boton-previo');
 const botonSiguiente = document.getElementById('boton-siguiente');
 
-// función para actualizar offset y pedir pokemones
-function actualizarListaPokemones(cambio) {
-    offset = Math.max(0, offset + cambio); // nunca bajar de 0
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`;
-    obtenerListaPokemones(url);
+
+function actualizarListaPokemones(cambio){
+    offset = Math.max(0, offset + cambio); 
+    obtenerListaPokemones(offset)
 }
+
 
 botonPrevio.addEventListener('click', () => manejarClickPrevio(actualizarListaPokemones));
 botonSiguiente.addEventListener('click', () => manejarClickSiguiente(actualizarListaPokemones));
